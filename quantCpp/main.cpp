747583ed8731c5c6a84457d42eda6161858af6e1
Geyijie <granthst@hotmail.com>
2017-12-13 00:34:53 +0800

    Initial Commit

---
+++ quantCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  quantCpp
+//
+//  Created by Geyijie on 13/12/2017.
+//  Copyright © 2017 Geyijie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
