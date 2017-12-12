3151f600bfb768f286a28778031732702c86b301
Geyijie <granthst@hotmail.com>
2017-12-13 00:44:33 +0800

    update main.cpp

--- quantCpp/main.cpp
+++ quantCpp/main.cpp
@@ -12,6 +12,6 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     VanillaOption v;
-    cout << v.getK() <<endl;
+    cout << v.calc_put_price() <<endl;
     return 0;
 }
